f5cab445e3b7df2065b661934089a4683566fe91
Darriel Markerizal <darrielmarkerizal@gmail.com>
2024-01-01 18:05:33 +0700

    clerk authentication

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+const SetupPage = () => {
+    return (
+        <div className="p-4">
+            <UserButton afterSignOutUrl="/" />
+        </div>
+    );
+};
+
+export default SetupPage;
